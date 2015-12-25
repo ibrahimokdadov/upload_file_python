b62cc1bc3e1eb0cfa4b8176208d424ba3526d0c7
ibininja <ibrahim.mokdad@gmail.com>
2015-12-25 13:54:00 +0300

    Upload Script
    Upload Files using python and flask

---
+++ src/app.py
@@ -0,0 +1,32 @@
+import os
+from flask import Flask, render_template, request
+
+__author__ = 'ibininja'
+
+app = Flask(__name__)
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+@app.route("/")
+def index():
+    return render_template("upload.html")
+
+@app.route("/upload", methods=['POST'])
+def upload():
+    target = os.path.join(APP_ROOT, 'images/')
+    print(target)
+
+    if not os.path.isdir(target):
+        os.mkdir(target)
+
+    for file in request.files.getlist("file"):
+        print(file)
+        filename = file.filename
+        destination = "/".join([target, filename])
+        print(destination)
+        file.save(destination)
+
+    return render_template("complete.html")
+
+if __name__ == "__main__":
+    app.run(port=4555, debug=True)
