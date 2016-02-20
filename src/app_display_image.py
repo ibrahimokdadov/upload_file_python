53c624148887f025288d67be63c03657234beeda
ibininja <ibrahim.mokdad@gmail.com>
2016-02-20 13:42:12 +0300

    added code for displaying uploaded image

---
+++ src/app_display_image.py
@@ -0,0 +1,46 @@
+import os
+from uuid import uuid4
+
+from flask import Flask, request, render_template, send_from_directory
+
+__author__ = 'ibininja'
+
+app = Flask(__name__)
+# app = Flask(__name__, static_folder="images")
+
+
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+@app.route("/")
+def index():
+    return render_template("upload.html")
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    target = os.path.join(APP_ROOT, 'images/')
+    # target = os.path.join(APP_ROOT, 'static/')
+    print(target)
+    if not os.path.isdir(target):
+            os.mkdir(target)
+    else:
+        print("Couldn't create upload directory: {}".format(target))
+    print(request.files.getlist("file"))
+    for upload in request.files.getlist("file"):
+        print(upload)
+        print("{} is the file name".format(upload.filename))
+        filename = upload.filename
+        destination = "/".join([target, filename])
+        print ("Accept incoming file:", filename)
+        print ("Save it to:", destination)
+        upload.save(destination)
+
+    # return send_from_directory("images", filename, as_attachment=True)
+    return render_template("complete.html", image_name=filename)
+
+@app.route('/upload/<filename>')
+def send_image(filename):
+    return send_from_directory("images", filename)
+
+if __name__ == "__main__":
+    app.run(port=4555, debug=True)
