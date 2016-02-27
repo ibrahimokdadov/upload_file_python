6d11b60579e535ddc4265864fb64429a42578bf1
ibininja <ibrahim.mokdad@gmail.com>
2016-02-27 11:04:16 +0300

    pushed upload to upload to specific folder

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, render_template, request
+
+from flask import Flask, request, render_template, send_from_directory
 
 __author__ = 'ibininja'
 
@@ -7,26 +8,55 @@ app = Flask(__name__)
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+
 @app.route("/")
 def index():
     return render_template("upload.html")
 
-@app.route("/upload", methods=['POST'])
-def upload():
-    target = os.path.join(APP_ROOT, 'images/')
-    print(target)
 
+@app.route("/upload", methods=["POST"])
+def upload():
+    folder_name = request.form['superhero']
+    '''
+    # this is to verify that folder to upload to exists.
+    if os.path.isdir(os.path.join(APP_ROOT, 'files/{}'.format(folder_name))):
+        print("folder exist")
+    '''
+    target = os.path.join(APP_ROOT, 'files/{}'.format(folder_name))
+    print(target)
     if not os.path.isdir(target):
         os.mkdir(target)
-
-    for file in request.files.getlist("file"):
-        print(file)
-        filename = file.filename
+    print(request.files.getlist("file"))
+    for upload in request.files.getlist("file"):
+        print(upload)
+        print("{} is the file name".format(upload.filename))
+        filename = upload.filename
+        # This is to verify files are supported
+        ext = os.path.splitext(filename)[1]
+        if (ext == ".jpg") or (ext == ".png"):
+            print("File supported moving on...")
+        else:
+            render_template("Error.html", message="Files uploaded are not supported...")
         destination = "/".join([target, filename])
-        print(destination)
-        file.save(destination)
+        print("Accept incoming file:", filename)
+        print("Save it to:", destination)
+        upload.save(destination)
 
-    return render_template("complete.html")
+    # return send_from_directory("images", filename, as_attachment=True)
+    return render_template("complete.html", image_name=filename)
+
+
+@app.route('/upload/<filename>')
+def send_image(filename):
+    return send_from_directory("images", filename)
+
+
+@app.route('/gallery')
+def get_gallery():
+    image_names = os.listdir('./images')
+    print(image_names)
+    return render_template("gallery.html", image_names=image_names)
+
 
 if __name__ == "__main__":
     app.run(port=4555, debug=True)
